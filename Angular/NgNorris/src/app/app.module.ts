1e39d49695beaba7ab8fcbe3b9bf3a489567ef52
Carter Barnes <carter.barnes@cremadevelopment.com>
2018-02-08 19:52:19 -0800

    Add FactFinder service

--- Angular/NgNorris/src/app/app.module.ts
+++ Angular/NgNorris/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FactFinderService } from './fact-finder.service';
 
 
 @NgModule({
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [FactFinderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
